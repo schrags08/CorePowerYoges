f1615135548c2f0ae64bd61704c2d332898429d1
schrags08 <schrags08@hotmail.com>
2014-04-22 21:47:08 -0500

    add Business Logic layer to solution

---
+++ CorePowerYoges/CorePowerYoges.BLL/StateBL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorePowerYoges.BLL
+{
+    public class StateBL
+    {
+        public List<State> GetAllStates()
+        {
+        }
+    }
+}
